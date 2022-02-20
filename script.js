211fad4e7938294b9be5491ae7813b9a8b1f75e8
Hamid404 <hamidrmd.59@gmail.com>
2022-02-20 21:03:57 +0100

    scrollDisplaySetting

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let photosArray
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
 
 //Unsplash API
 const count=30;
 const apiKey='7I8RpZRSQJbAqXUUsNy7MuIg-eN9jW-bI52iL8IyAzY';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-
+//Check if all images were loaded
+function imageLoaded() {
+  //console.log('image loaded');  
+  imagesLoaded++;
+  //console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+      ready = true;
+      loader.hidden = true;
+      //console.log('ready =',ready);
+  }
+};
 
 //Helper function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -20,6 +33,9 @@ function setAttributes(element, attributes) {
 
 //Create Elements for Links & Photos, Add to Dom
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    //console.log('total images', totalImages);
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
@@ -40,13 +56,13 @@ function displayPhotos() {
               alt : photo.alt_description,
               title : photo.alt_description,
         });
+
+        //Event Listener, check when each is finished loading
+        img.addEventListener('load',imageLoaded);
+
         //Put <img> inside <a>,und than put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
-
-        
-
-
     });
 };
 
@@ -60,7 +76,7 @@ async function getPhotos() {
         //const data = await response.json();
         photosArray = await response.json();
         //console.log(data);
-        console.log(photosArray);
+        //console.log(photosArray);
         displayPhotos();
     } catch (error) {
         //Catch Error Here
@@ -69,12 +85,14 @@ async function getPhotos() {
 
 //Check to see if scrolling near bottom of page, Load more Photos
 window.addEventListener('scroll', ()=>{
-    //console.log('scrolled');
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
-          getPhotos();
-         //console.log('load more'); 
-    }
-})
+   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+       ready = false;
+       getPhotos();
+       //console.log('load more');
+       
+   }
+});
+
 
 //On Load
 getPhotos();
